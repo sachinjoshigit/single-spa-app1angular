28964ff02865408167e50078818cc2efd7ac4124
shweta pratapwar <shweta.pratapwar@gmail.com>
2020-02-24 19:59:55 +0530

    Adding all remaining initial files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { SampleComponent } from './sample/sample.component';
+
+const routes: Routes = [
+  { path: 'sample', component: SampleComponent },
+  { path: '**', component: EmptyRouteComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}]
+})
+export class AppRoutingModule { }
